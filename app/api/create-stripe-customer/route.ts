59d3f5ca6b090678678cba0b3f1cf5af5e27a458
sanofc <sanofc@gmail.com>
2025-01-30 15:30:10 +0900

    Enhance error handling and logging in Stripe customer creation API

--- app/api/create-stripe-customer/route.ts
+++ app/api/create-stripe-customer/route.ts
@@ -2,10 +2,11 @@ import { createClient } from "@/utils/supabase/server";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
-
 export async function POST(req: NextRequest) {
-
-  if(req.nextUrl.searchParams.get("API_ROUTE_SECRET") !== process.env.API_ROUTE_SECRET) {
+  if (
+    req.nextUrl.searchParams.get("API_ROUTE_SECRET") !==
+    process.env.API_ROUTE_SECRET
+  ) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
@@ -14,21 +15,30 @@ export async function POST(req: NextRequest) {
 
     const { record } = body;
 
+    console.log("Record:", record);
+
     console.log("Creating Stripe customer for:", record.email);
 
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY! );
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
     // Stripeカスタマーの作成
     const customer = await stripe.customers.create({
       email: record.email,
     });
 
-
     const supabase = await createClient();
     // SupabaseにStripeカスタマーIDを保存
-    await supabase
+    const { error } = await supabase
       .from("profile")
       .update({ stripe_customer: customer.id })
       .eq("id", record.id);
+
+    if (error) {
+      console.error("Supabase error:", error);
+      return NextResponse.json(
+        { error: "Internal Server Error" },
+        { status: 500 }
+      );
+    }
 
     return NextResponse.json(
       { message: `Stripe customer created: ${customer.id}` },
@@ -36,6 +46,9 @@ export async function POST(req: NextRequest) {
     );
   } catch (error) {
     console.error("Error:", error);
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
